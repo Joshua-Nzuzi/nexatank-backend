76ce10302273aa442dfb79ecb6b4194fc945a799
Joshua Nzuzi <nzuzijoshua@gmail.com>
2026-03-18 01:57:12 +0100

    feat: fix 404 error and CommonJS rate

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-export const development = {
+const development = {
   client: 'pg',
   connection: {
     host: process.env.DB_HOST,
@@ -8,7 +8,7 @@ export const development = {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    // AJOUT CRUCIAL : Active le SSL si on ne pointe pas sur localhost
+    // Active SSL si on ne pointe pas sur localhost
     ssl: process.env.DB_HOST !== 'localhost' && process.env.DB_HOST !== '127.0.0.1'
       ? { rejectUnauthorized: false }
       : false
@@ -17,7 +17,8 @@ export const development = {
     directory: './migrations'
   }
 };
-export const production = {
+
+const production = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
   // Toujours SSL en production (Railway)
@@ -26,3 +27,5 @@ export const production = {
     directory: './migrations'
   }
 };
+
+module.exports = { development, production };
